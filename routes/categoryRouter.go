b4c1711cf05d16839a2dc01896965af7ba862e8c
JairDavid <jairvasmar@gmail.com>
2021-10-21 21:44:20 -0500

    routes modified, the patch method does not works yet

--- routes/categoryRouter.go
+++ routes/categoryRouter.go
@@ -11,13 +11,8 @@ func CategoryRouter(api *gin.RouterGroup) {
 	category := *api.Group("/category")
 	categoryService := service.NewCategoryRepository()
 
-	category.POST("/", func(c *gin.Context) {
-		categoryObj, err := categoryService.Create(c)
-		if err != nil {
-			c.JSON(http.StatusCreated, gin.H{"data": err.Error()})
-		} else {
-			c.JSON(http.StatusCreated, gin.H{"data": categoryObj})
-		}
+	category.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": categoryService.GetAll()})
 	})
 
 	category.GET("/:id", func(c *gin.Context) {
@@ -29,11 +24,16 @@ func CategoryRouter(api *gin.RouterGroup) {
 		}
 	})
 
-	category.GET("/", func(c *gin.Context) {
-		categoryObjs := categoryService.GetAll()
-		c.JSON(http.StatusOK, gin.H{"data": categoryObjs})
+	category.POST("/", func(c *gin.Context) {
+		categoryObj, err := categoryService.Create(c)
+		if err != nil {
+			c.JSON(http.StatusCreated, gin.H{"data": err.Error()})
+		} else {
+			c.JSON(http.StatusCreated, gin.H{"data": categoryObj})
+		}
 	})
 
+	//testing it does not work yet
 	category.PATCH("/", func(c *gin.Context) {
 		categoryService.Update(c)
 	})
