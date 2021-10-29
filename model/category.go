78524a8b5ff455cb0ccce243993efc219d82ce3b
JairDavid <jairvasmar@gmail.com>
2021-10-29 12:40:30 -0500

    model updated

--- model/category.go
+++ model/category.go
@@ -4,8 +4,8 @@ import "github.com/jinzhu/gorm"
 
 type Category struct {
 	gorm.Model
-	Name        string `json:"name,omitempty" binding:"required" gorm:"type:varchar(20);not null"`
-	Description string `json:"description,omitempty" binding:"required" gorm:"type:varchar(50);not null"`
+	Name        string `json:"name" binding:"required" gorm:"type:varchar(20);not null"`
+	Description string `json:"description" binding:"required" gorm:"type:varchar(50);not null"`
 
 	//add foreing key at the migration, reference(category_id)
 	Products []Product
